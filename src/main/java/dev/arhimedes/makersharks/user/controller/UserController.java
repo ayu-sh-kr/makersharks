d8c90594bfcc70737e7209183852da237a329b45
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-21 21:13:54 +0530

    Update UserController.java

--- src/main/java/dev/arhimedes/makersharks/user/controller/UserController.java
+++ src/main/java/dev/arhimedes/makersharks/user/controller/UserController.java
@@ -2,11 +2,15 @@ package dev.arhimedes.makersharks.user.controller;
 
 import dev.arhimedes.makersharks.helper.api.APIResponse;
 import dev.arhimedes.makersharks.user.dtos.UserRegistrationDTO;
+import dev.arhimedes.makersharks.user.entity.UserInfoDTO;
 import dev.arhimedes.makersharks.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -17,18 +21,18 @@ public class UserController {
 
 
     @PostMapping
-    public ResponseEntity<APIResponse> createUser(@RequestBody @Validated UserRegistrationDTO userRegistrationDTO){
+    public ResponseEntity<APIResponse<String>> createUser(@RequestBody @Validated UserRegistrationDTO userRegistrationDTO){
         return userService.create(userRegistrationDTO);
     }
 
 
     @GetMapping
-    public ResponseEntity<APIResponse> getAll(){
+    public ResponseEntity<APIResponse<List<UserInfoDTO>>> getAll(){
         return userService.fetchAllUsers();
     }
 
     @GetMapping("/page")
-    public ResponseEntity<APIResponse> getAllByPage(@RequestParam("page") int page, @RequestParam("size") int size){
+    public ResponseEntity<APIResponse<Page<UserInfoDTO>>> getAllByPage(@RequestParam("page") int page, @RequestParam("size") int size){
         return userService.fetchWithPage(page, size);
     }
 

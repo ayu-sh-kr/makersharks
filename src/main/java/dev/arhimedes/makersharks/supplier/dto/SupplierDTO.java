d842314dfeb36b7eaf2f314852e7e34fc3cc3184
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-21 21:16:55 +0530

    Create SupplierDTO.java: DTO for fetching and creating Supplier records

---
+++ src/main/java/dev/arhimedes/makersharks/supplier/dto/SupplierDTO.java
@@ -0,0 +1,33 @@
+package dev.arhimedes.makersharks.supplier.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.arhimedes.makersharks.supplier.enums.BusinessNature;
+import dev.arhimedes.makersharks.supplier.enums.ManufacturingProcess;
+import dev.arhimedes.makersharks.supplier.utils.validators.annotations.EnumPattern;
+import jakarta.validation.constraints.NotBlank;
+
+public record SupplierDTO(
+
+    Integer id,
+
+    @NotBlank(message = "Company name can't be blank")
+    @JsonProperty("company_name")
+    String company,
+
+    @NotBlank(message = "Website can't be blank")
+    String website,
+
+    @NotBlank(message = "Location can't be blank")
+    String location,
+
+    @NotBlank(message = "Nature can't be blank")
+    @EnumPattern(enumClass = BusinessNature.class)
+    @JsonProperty("business_nature")
+    String nature,
+
+    @NotBlank(message = "Process can't be blank")
+    @EnumPattern(enumClass = ManufacturingProcess.class)
+    @JsonProperty("manufacturing_process")
+    String process
+) {
+}
